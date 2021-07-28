d4af2c7f77140c5e1275cdaf6ff3ff3741399b0e
qangelot <q.angelot@gmail.com>
2021-07-28 12:39:46 +0200

    refactring error handling and adding new templates: 400, 403, 405.

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_wtf.csrf import CSRFProtect
+import logging
+from flask.logging import default_handler
+from logging.handlers import RotatingFileHandler
 
 
-
-# instantiate plugins globally
+# Instantiate plugins globally
 db = SQLAlchemy()
 maill = Mail()
 login_manager = LoginManager()
@@ -14,7 +16,9 @@ login_manager = LoginManager()
 
 def create_app():
     """Initialize the core application."""
+
     app = Flask(__name__, instance_relative_config=False)
+    # Configure the flask app instance
     app.config.from_object('config.DevConfig')
 
     # Initialize Plugins
@@ -28,15 +32,45 @@ def create_app():
         from .canteen_bi import bi_routes
         from .canteen_predict import predict_routes
         from .home import routes
-        from . import errors
 
         # Register Blueprints
         app.register_blueprint(bi_routes.bi_bp)
         app.register_blueprint(predict_routes.predict_bp)        
         app.register_blueprint(routes.home_bp)
-        app.register_blueprint(errors.errors_bp)
+
+        # Register error handlers
+        register_error_handlers(app)
 
         # Create Database Models
         db.create_all()
 
         return app
+
+
+# customized error handlers
+def register_error_handlers(app):
+
+    # 400 - Bad Request
+    @app.errorhandler(400)
+    def bad_request(e):
+        return render_template('400.html'), 400
+
+    # 403 - Forbidden
+    @app.errorhandler(403)
+    def forbidden(e):
+        return render_template('403.html'), 403
+
+    # 404 - Page Not Found
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
+
+    # 405 - Method Not Allowed
+    @app.errorhandler(405)
+    def method_not_allowed(e):
+        return render_template('405.html'), 405
+
+    # 500 - Internal Server Error
+    @app.errorhandler(500)
+    def server_error(e):
+        return render_template('500.html'), 500
