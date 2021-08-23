8c4fefd5e2fa4c5440fb70d62d114b9892c9c74d
qangelot <q.angelot@gmail.com>
2021-08-23 12:22:39 +0200

    changes to config file

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = 'qadev54@gmail.com'
+    MAIL_USERNAME = os.environ.get('MAIL_DEFAULT_SENDER')
     MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
     MAIL_SUPPRESS_SEND = False
 
